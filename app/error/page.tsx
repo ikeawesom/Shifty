f3c8a26fd12a6ea3442990d14e4c72f5496c1752
ikeawesom <ikelimjl@gmail.com>
2024-04-13 15:43:03 +0800

    kindeAuth routes set up

---
+++ app/error/page.tsx
@@ -0,0 +1,35 @@
+import CenterContainer from "@/src/components/CenterContainer";
+import FlexContainer from "@/src/components/utils/FlexContainer";
+import { DEFAULT_ICON_SIZE } from "@/src/constants";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+import React from "react";
+
+export default function ErrorPage({
+  searchParams,
+}: {
+  searchParams: { [query: string]: string };
+}) {
+  const { code, message } = searchParams;
+  if (!code || !message) redirect("/");
+
+  return (
+    <CenterContainer className="min-h-svh">
+      <FlexContainer className="gap-5 flex-col">
+        <Image
+          src="/icons/icon_hm.svg"
+          alt="An error has occured"
+          width={DEFAULT_ICON_SIZE * 5}
+          height={DEFAULT_ICON_SIZE * 5}
+        />
+        <FlexContainer className="gap-4 flex-col text-4xl">
+          <h1 className="font-bold text-blue-500">
+            Oops, an error has occured.
+          </h1>
+          <h1 className="font-bold text-gray-700">Please try again later.</h1>
+          <p className="text-lg text-gray-500">{`ERROR ${code}: ${message}`}</p>
+        </FlexContainer>
+      </FlexContainer>
+    </CenterContainer>
+  );
+}
