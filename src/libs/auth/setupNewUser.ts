ae0ab62719d54c481887a7392d9e0b1a6e11124f
ikeawesom <ikelimjl@gmail.com>
2024-04-13 15:44:15 +0800

    created utility functions

---
+++ src/libs/auth/setupNewUser.ts
@@ -0,0 +1,28 @@
+import { KindeUser } from "@kinde-oss/kinde-auth-nextjs/types";
+import client from "../prisma/prismadb";
+import { prismaConnect } from "../prisma";
+import handleResponses from "../handleResponses";
+
+export async function setupNewUser(user: KindeUser) {
+  try {
+    await prismaConnect();
+    const data = await client.user.findUnique({
+      where: { auth_id: user.id ?? "" },
+    });
+
+    if (data === null) {
+      // not found
+      await client.user.create({
+        data: {
+          auth_id: user.id,
+          email: user.email ?? "",
+          first_name: user.given_name ?? "",
+          last_name: user.family_name ?? "",
+        },
+      });
+    }
+    return handleResponses();
+  } catch (error: any) {
+    return handleResponses({ status: false, error });
+  }
+}
