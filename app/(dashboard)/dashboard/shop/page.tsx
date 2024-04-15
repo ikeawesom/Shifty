82a99e2648aee8c0daf6229ddedd94c956c28c3b
ikeawesom <ikelimjl@gmail.com>
2024-04-15 23:35:47 +0800

    created shop page

---
+++ app/(dashboard)/dashboard/shop/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Shop",
+};
+
+export default function Shop() {
+  return <div>Shop Page</div>;
+}
