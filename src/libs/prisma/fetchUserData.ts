386a8d831efd75c1e6efaba83e2fcc2e93f22723
ikeawesom <ikelimjl@gmail.com>
2024-04-15 23:34:37 +0800

    utility functions to fetch user data

---
+++ src/libs/prisma/fetchUserData.ts
@@ -0,0 +1,65 @@
+import { redirect } from "next/navigation";
+import { prismaConnect, prismaDisconnect } from ".";
+import { getServerSession } from "../auth/getServerSession";
+import handleResponses from "../handleResponses";
+import client from "./prismadb";
+
+export async function getAuthID() {
+  try {
+    await prismaConnect();
+
+    const {
+      user: { email },
+    } = await getServerSession();
+
+    const auth_id = await client.user.findUnique({
+      where: {
+        email: email ?? "",
+      },
+      select: {
+        auth_id: true,
+      },
+    });
+
+    if (!auth_id || !email)
+      redirect(
+        `/error?${new URLSearchParams({
+          code: `404`,
+          message: "Invalid email address. Please sign out and try again.",
+        })}`
+      );
+
+    return handleResponses({ data: auth_id.auth_id });
+  } catch (error: any) {
+    return handleResponses({ status: false, error });
+  }
+}
+
+export async function fetchCurrentEvents(auth_id: string) {
+  try {
+    await prismaConnect();
+    const userPlansData = await client.plans.findMany({
+      where: { createdBy: auth_id },
+    });
+    return handleResponses({ data: userPlansData });
+  } catch (error: any) {
+    return handleResponses({ status: false, error });
+  } finally {
+    await prismaDisconnect();
+  }
+}
+
+export async function fetchUserPoints(auth_id: string) {
+  try {
+    await prismaConnect();
+    const pointsData = await client.user.findUnique({
+      where: { auth_id },
+      select: { points: true },
+    });
+    return handleResponses({ data: pointsData?.points });
+  } catch (error: any) {
+    return handleResponses({ status: false, error });
+  } finally {
+    await prismaDisconnect();
+  }
+}
