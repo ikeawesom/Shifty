82e55ec75e43d1e5ff2221f51e572f5d49ba830c
ikeawesom <ikelimjl@gmail.com>
2024-04-25 19:30:33 +0800

    members table component

--- src/components/dashboard/members/MembersTable.tsx
+++ src/components/dashboard/members/MembersTable.tsx
@@ -7,10 +7,16 @@ import Spinner from "../../utils/graphics/Spinner";
 import { useMembers } from "@/src/hooks/useMembers";
 import GraphicsEmpty from "../../utils/graphics/Empty";
 import AddMembersOptions from "./AddMembersOptions";
+import HRow from "../../utils/HRow";
+import Link from "next/link";
+import Points from "../../utils/graphics/Points";
 
 export default function MembersTable({ className }: UtilityType) {
   const { loading, members, onQueryChange, query } = useMembers();
   const empty = members?.length === 0;
+
+  const getRoute = (id: string) =>
+    `/dashboard/members?${new URLSearchParams({ view: id })}`;
 
   return (
     <>
@@ -25,6 +31,7 @@ export default function MembersTable({ className }: UtilityType) {
           />
           <AddMembersOptions />
         </form>
+        <HRow />
         <div
           className={twMerge(
             "w-full",
@@ -33,25 +40,60 @@ export default function MembersTable({ className }: UtilityType) {
         >
           {loading ? (
             <Spinner />
-          ) : empty ? (
+          ) : empty && query === "" ? (
             <GraphicsEmpty type="sleeping">
               This place looks empty. Try inviting others or create members!
             </GraphicsEmpty>
+          ) : empty && query !== "" ? (
+            <GraphicsEmpty type="question" size={250}>
+              Hmmm, member "{query}" cannot be found. Try check your spelling or
+              create a new member!
+            </GraphicsEmpty>
           ) : (
-            members &&
-            members.map((member: MemberType) => {
-              const { member_id, first_name, last_name, points } = member;
-              const full_name = `${first_name} ${last_name}`;
-              return (
-                <tr
-                  className="flex items-center justify-between p-3"
-                  key={member_id}
-                >
-                  <p>{full_name}</p>
-                  <p>{points}</p>
-                </tr>
-              );
-            })
+            members && (
+              <>
+                <table>
+                  <tbody>
+                    <tr className="header">
+                      <th>Name</th>
+                      <th>
+                        <span className="flex items-center justify-start gap-0">
+                          Points <Points />
+                        </span>
+                      </th>
+                    </tr>
+                    {members.map((member: MemberType) => {
+                      const { member_id, first_name, last_name, points } =
+                        member;
+                      const full_name = `${first_name} ${last_name}`;
+                      if (member_id)
+                        return (
+                          <tr key={member_id}>
+                            <td className="custom">
+                              <Link
+                                className="block custom p-3"
+                                href={getRoute(member_id)}
+                              >
+                                {full_name}
+                              </Link>
+                            </td>
+                            <td className="custom">
+                              <Link
+                                className="block custom p-3"
+                                href={getRoute(member_id)}
+                              >
+                                <span className="flex items-center justify-start gap-0">
+                                  {points} <Points />
+                                </span>
+                              </Link>
+                            </td>
+                          </tr>
+                        );
+                    })}
+                  </tbody>
+                </table>
+              </>
+            )
           )}
         </div>
       </FlexContainer>
