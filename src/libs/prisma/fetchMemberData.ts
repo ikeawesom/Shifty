f27eddbf3e8c10b351c157a6cfc39a54418c436f
ikeawesom <ikelimjl@gmail.com>
2024-04-25 16:00:15 +0800

    new server function to create new member

--- src/libs/prisma/fetchMemberData.ts
+++ src/libs/prisma/fetchMemberData.ts
@@ -14,3 +14,28 @@ export async function getMemberData(member_id: string) {
     await prismaDisconnect();
   }
 }
+
+export async function createNewMember(
+  first_name: string,
+  last_name: string,
+  points: number,
+  auth_id: string
+) {
+  try {
+    await prismaConnect();
+    const { member_id } = await client.member.create({
+      data: {
+        first_name,
+        last_name,
+        points,
+        createdBy: auth_id,
+        createdOn: new Date(),
+      },
+    });
+    return handleResponses({ data: member_id });
+  } catch (error: any) {
+    return handleResponses({ status: false, error });
+  } finally {
+    await prismaDisconnect();
+  }
+}
